a23753c060e9d7cef2ebfc3921035450d71f8967
Aleksandr <male225@mail.ru>
2021-02-20 19:28:40 +0600

    TSIS3 completed

--- week3/day1/11.py
+++ week3/day1/11.py
@@ -1,8 +1,11 @@
-l = [j for j in input().split()]
+import sys
+l = (str(sys.stdin.read()).split())
 d = {}
 for i in l:
 	if i in d.keys(): d[i] += 1
 	else: d[i] = 1
 q = list(d.items())
-q.sort(key = lambda i: i[1])
-for i in q: print(i.key)
+q.sort()
+q.sort(reverse = True, key = lambda i: i[1])
+for i in q:
+	print(i[0])
